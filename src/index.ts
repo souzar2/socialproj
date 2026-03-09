2bc05745d476414b200bc7cc12e234d24a5b9aa1
souzar2 <arthursilvasouza98@gmail.com>
2026-03-09 14:02:02 -0300

    implementando rotina

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import "reflect-metadata";
 import express from "express";
 import { AppDataSource } from "./data-source";
 import bodyParser from "body-parser";
+import cron from "node-cron";
 
 import Requests from "./paths/Requests"
+import { Post } from "./entities/Post";
+import { LessThan } from "typeorm";
 
 const port = 4000
 const app = express();
@@ -14,6 +17,16 @@ app.use("/", Requests);
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+
+//Rotina
+cron.schedule("*/30 * * * *", async () => {
+  console.log("Executando rotina a cada 30 minutos:", new Date());
+  const result = await AppDataSource.getRepository(Post).find({
+    where: {
+      
+    }
+  })
+});
 
 AppDataSource.initialize()
   .then(() => {
